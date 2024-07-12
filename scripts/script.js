6d9e692916a20f569a58a6a12e7aaad6dd5dc892
Avi <93165148+avi4h@users.noreply.github.com>
2024-07-12 15:24:41 +0530

    add lazy loading to posts

--- scripts/script.js
+++ scripts/script.js
@@ -41,7 +41,7 @@ function renderPostCard() {
 function postCardHtml() {
     const postHTML = postsCurrent.map(function(post) {
       return `<div class="post-div" class-post="${post.id}">
-          <img class="post-img" src="${post.imageLocation}" alt="${post.title}" loading="lazy">
+          <img class="post-img" src="${post.imageLocation}" alt="${post.title}">
           <p class="post-date">${post.date}</p>
           <h3 class="post-head"><a href="/pages/blog${post.id}.html">${post.title}</a></h3>
           <p class="post-desc">${post.abstract}</p>
